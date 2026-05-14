ef2e69f678f10f3d2b942e282007909b0c571bce
davidjavila-web <davidjavila@aol.com>
2026-05-14 08:27:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='e6b-v17';
-const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
+const CACHE='e6b-v19';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(nr=>{if(nr.ok&&e.request.method==='GET'){const c=nr.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));}return nr;}).catch(()=>caches.match('/index.html'))));});
